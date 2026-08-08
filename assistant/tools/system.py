c219a8aefb9e1a4acded4359b3107e82cdb25804
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2026-08-08 10:48:46 +0300

    Fix an annotation for get_os_info() function

--- assistant/tools/system.py
+++ assistant/tools/system.py
@@ -3,7 +3,7 @@ import platform
 from agents import function_tool
 
 
-def get_os_info() -> str:
+def get_os_info() -> dict[str, str]:
     """Returns system info including os, version, release, machine, version of python"""
     return {
         "system": platform.system(),
